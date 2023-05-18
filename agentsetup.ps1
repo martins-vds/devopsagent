c4b638f11d1927e4aec0a6332e141c25e9f4eeac
Roberto Borges <roberto.borges@outlook.com>
2023-05-18 11:33:01 -0400

    Removing GIT from windows already there

--- agentsetup.ps1
+++ agentsetup.ps1
@@ -25,8 +25,6 @@ function setupazdevops{
     
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-    choco install git -y
-
     choco install openjdk -y
 
     choco install nodejs.install -y
