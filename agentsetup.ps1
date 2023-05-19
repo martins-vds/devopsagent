697beef18082908fc0a8deba0f1987ebfaa3767b
Roberto Borges <roberto.borges@outlook.com>
2023-05-19 11:21:00 -0400

    Updating Choco Path

--- agentsetup.ps1
+++ agentsetup.ps1
@@ -25,6 +25,8 @@ function setupazdevops{
     
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+    $env:PATH += ";%ALLUSERSPROFILE%\chocolatey\bin" 
+    
     choco install oraclejdk -y
 
     choco install nodejs.install -y
