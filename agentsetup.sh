3e2a77d3cd3af95b2719eb63442e7e7c8951bad5
Roberto Borges <roberto.borges@outlook.com>
2024-03-13 12:39:28 -0400

    updating to have docker

--- agentsetup.sh
+++ agentsetup.sh
@@ -22,6 +22,18 @@ setup_az_devops() {
     sudo apt-get update
     sudo apt-get install -y openjdk-11-jdk nodejs npm
 
+    sudo apt install apt-transport-https ca-certificates curl software-properties-common
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+    apt-cache policy docker-ce
+
+    sudo apt install docker-ce
+
+    sudo systemctl status docker
+    
     echo "About to setup Azure DevOps Agent"
     azagentdir="/agent"
 
