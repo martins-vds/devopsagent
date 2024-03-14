0576c57521bdad546b9197ad048b77d24fc92752
Roberto Borges <roberto.borges@outlook.com>
2024-03-13 22:26:14 -0400

    Update

--- agentsetup.sh
+++ agentsetup.sh
@@ -71,61 +71,22 @@ setup_az_devops() {
 
     sudo apt install docker-ce -y
 
-    # sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    sudo -u LabUser curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-    # nvm install v20.11.0
+    export NVM_DIR="LabUser/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-    # npm install -g npm@6.14.4
+    nvm install v20.11.0
 
-    sudo usermod -aG docker $USER
+    npm install -g npm@6.14.4
+
+    sudo usermod -aG docker LabUser
 
     newgrp docker
 
 }
 
-setup_gh_runner() {
-    URL=$1
-    PAT=$2
-    POOL=$3
-    AGENT=$4
-
-    echo "About to setup GitHub Runner"
-    ghrunnerdirectory="/actions-runner"
-
-    # Test if an old installation exists, if so, delete the folder
-    if [ -d "$ghrunnerdirectory" ]; then
-        cd $ghrunnerdirectory
-        servicename=$(cat .service)
-        sudo systemctl stop $servicename
-        cd /
-        sudo rm -rf $ghrunnerdirectory
-    fi
-
-    # Create a new folder
-    mkdir -p $ghrunnerdirectory
-    cd $ghrunnerdirectory
-
-    # Get the latest build agent version
-    tag=$(curl -s https://api.github.com/repos/actions/runner/releases/latest | grep tag_name | head -1 | sed -E 's/.*"v([^"]+)".*/\1/')
-    echo "$tag is the latest version"
-    download="https://github.com/actions/runner/releases/download/v$tag/actions-runner-linux-x64-$tag.tar.gz"
-    curl -L $download -o ghactionsrunner.tar.gz
-
-    # Expand the tarball
-    tar -zxvf ghactionsrunner.tar.gz
-
-    # Run the config script of the build agent
-    echo "Configuring GitHub Runner"
-    ./config.sh --unattended --url "https://github.com/$URL" --token "$PAT" --runnergroup "$POOL" --replace
-
-    ./svc.sh install
-    ./svc.sh start
-}
-
 echo "Parameters: URL=$URL, PAT=$PAT, POOL=$POOL, AGENT=$AGENT, AGENTTYPE=$AGENTTYPE"
 
-if [ "${AGENTTYPE,,}" == "azuredevops" ]; then
-    setup_az_devops $URL $PAT $POOL $AGENT
-else
-    setup_gh_runner $URL $PAT $POOL $AGENT
-fi
+setup_az_devops $URL $PAT $POOL $AGENT
